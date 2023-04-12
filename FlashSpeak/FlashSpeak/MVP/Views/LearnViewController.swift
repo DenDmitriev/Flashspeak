6fe8cbc110a95ab54515eaa98dc9c024f8617d34
DenDmitriev <dv.denstr@gmail.com>
2023-04-12 13:48:16 +0300

    Add base project
    Base project with tab bar

---
+++ FlashSpeak/FlashSpeak/MVP/Views/LearnViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LearnViewController.swift
+//  Lingocard
+//
+//  Created by Denis Dmitriev on 12.04.2023.
+//
+
+import UIKit
+
+class LearnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FlashSpeak/FlashSpeak/MVP/Views/MainTabBarController.swift
@@ -0,0 +1,75 @@
+//
+//  MainTabBarController.swift
+//  Lingocard
+//
+//  Created by Denis Dmitriev on 12.04.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        UITabBar.appearance().barTintColor = .systemBackground
+        tabBar.tintColor = .label
+        setupViewControllers()
+        setupAppearance()
+        tabBar.safeAreaInsetsDidChange()
+    }
+    
+    func setupViewControllers() {
+        viewControllers = [
+            createNavigationController(
+                for: ListsViewController(),
+                title: NSLocalizedString("Списки слов", comment: ""),
+                image: UIImage(systemName: "square.stack.fill")),
+            createNavigationController(
+                for: LearnViewController(),
+                title: NSLocalizedString("Изучение", comment: ""),
+                image: UIImage(systemName: "play.square.stack.fill")),
+            createNavigationController(
+                for: StatisticsViewController(),
+                title: NSLocalizedString("Статистика", comment: ""),
+                image: UIImage(systemName: "chart.bar.xaxis"))
+        ]
+    }
+    
+    private func createNavigationController(for rootViewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = image
+        navigationController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navigationController
+    }
+    
+    private func setupAppearance() {
+        let positionOnX: CGFloat = 0
+        let positionOnY: CGFloat = 16
+        let width = tabBar.bounds.width - positionOnX * 2
+        let height = tabBar.bounds.height + positionOnY * 2
+        let roundlayer = CAShapeLayer()
+        
+        let bezierPath = UIBezierPath(
+            roundedRect: CGRect(
+                x: positionOnX,
+                y: tabBar.bounds.minY - positionOnY,
+                width: width,
+                height: height + positionOnY
+            ),
+            cornerRadius: positionOnY
+        )
+        
+        roundlayer.path = bezierPath.cgPath
+        
+        tabBar.layer.insertSublayer(roundlayer, at: 0)
+        tabBar.itemPositioning = .centered
+        
+        roundlayer.fillColor = UIColor.Theme.backgroundLightGray.cgColor
+        tabBar.tintColor = .Theme.tint
+        tabBar.unselectedItemTintColor = .Theme.tabBarUnselected
+    }
+
+}
